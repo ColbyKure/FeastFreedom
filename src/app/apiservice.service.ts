26265f37f59abe2164b08e7518856ea631b63f6f
Colby Kure <colbykure@gmail.com>
2020-07-21 13:24:57 -0700

    added service

---
+++ src/app/apiservice.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiserviceService {
+
+  constructor(private http: HttpClient) { }
+
+  rootURL = 'http://localhost:2000/api';
+
+  // userController functions
+  getUser() { 
+    return this.http.get(this.rootURL + '/users'); 
+  }
+  postUser(user:any) { 
+    return this.http.post(this.rootURL + '/users', user); 
+  }
+  putUser(user:any) { 
+    return this.http.put(this.rootURL + '/users/' + user._id, user); 
+  }
+  deleteUser(userid:String) {
+    return this.http.delete(this.rootURL + '/users/' + userid); 
+  }
+  getUserByID(userid:String) {
+    return this.http.get(this.rootURL + '/users/' + userid); 
+  }
+  getUserByEmail(email:String) {
+    return this.http.get(this.rootURL + '/users/email/' + email); 
+  }
+
+  // kitchenController functions
+  getKitchen() { 
+    return this.http.get(this.rootURL + '/kitchens'); 
+  }
+  postKitchen(kitchen:any) { 
+    return this.http.post(this.rootURL + '/kitchens', kitchen); 
+  }
+  putKitchen(kitchen:any) { 
+    return this.http.put(this.rootURL + '/kitchens/' + kitchen._id, kitchen); 
+  }
+  deleteKitchen(kitchenid:String) {
+    return this.http.delete(this.rootURL + '/kitchens/' + kitchenid); 
+  }
+  getKitchenByID(kitchenid:String) {
+    return this.http.get(this.rootURL + '/kitchens/' + kitchenid); 
+  }
+  getKitchenByEmail(email:String) {
+    return this.http.get(this.rootURL + '/kitchens/email/' + email); 
+  }
+
+  // authController functions
+  signupUser() {}
+  authenticateToken() {}
+  authorizeUser() {}
+}

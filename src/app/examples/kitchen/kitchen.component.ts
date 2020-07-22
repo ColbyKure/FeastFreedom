1927f6f5065bdce10a156b6e573881f9dba5589c
daiying98 <daiying98@gmail.com>
2020-07-21 20:06:38 -0400

    kitchen

---
+++ src/app/examples/kitchen/kitchen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './kitchen.component.html',
+  styleUrls: ['./kitchen.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

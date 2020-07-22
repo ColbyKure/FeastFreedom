3bee5989165932849b9c02fc6ccdc4eda5eae6b2
Colby Kure <colbykure@gmail.com>
2020-07-22 08:53:33 -0700

    kitchen and login

---
+++ src/app/models/kitchen.ts
@@ -0,0 +1,12 @@
+export class Kitchen {
+    public _id:any;
+    public KitchenName:String;
+    public Email:String;
+    public Password:String;
+    public KitchenType:Number;
+    public WorkingDays:String;
+    public OpenTime:String;
+    public CloseTime:String;
+    public ImagePath:String;
+    public items:any;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    public _id:any;
+    public UserName:String;
+    public Email:String;
+    public Password:String;
+    public Question1:Number;
+    public Answer1:String;
+    public Question2:Number;
+    public Answer2:String;
+}

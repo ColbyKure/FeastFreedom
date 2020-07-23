d5ce0974a2f23ecced4d4e115463afb0db98b5c6
Colby Kure <colbykure@gmail.com>
2020-07-23 12:38:22 -0700

    nav bar logout adn login

--- src/app/examples/homepage/homepage.component.ts
+++ src/app/examples/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    if (!localStorage.getItem('foo')) { 
+      localStorage.setItem('foo', 'no reload') 
+      location.reload() 
+    } else {
+      localStorage.removeItem('foo') 
+    }
+    if (localStorage.getItem('errorLogin')){
+      this.router.navigate(['/login']);
+    }
   }
 
 }

f8a37cb893029cb4748301de0eae1a1c5ce0d27c
Colby Kure <colbykure@gmail.com>
2020-07-22 19:22:58 -0700

    almost done with login

--- src/app/examples/login/login.component.ts
+++ src/app/examples/login/login.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   users:any;
   errMsg:string;
-  hide = true;
+  isUser = false;
   public uModel = new User();
 
   constructor(private uService:ApiserviceService, private router:Router) { }
@@ -24,23 +24,25 @@ export class LoginComponent implements OnInit {
     this.uService.authorizeUser(this.uModel).subscribe(
       (data) => {
         this.users = data;
-        console.log('setting token in local storage: "currUserToken"')
-        console.log(this.users.token)
         localStorage.setItem('currUserToken', JSON.stringify(this.users.token));
-        console.log('setting name in local storage: "currUserName"')
-        console.log(this.users.user.UserName);
         localStorage.setItem('currUserName', JSON.stringify(this.users.user.UserName));
-        console.log('setting id in local storage: "currUserID"')
-        console.log(this.users.user._id);
         localStorage.setItem('currUserID', JSON.stringify(this.users.user._id));
       },
       (error) => this.errMsg = error
     )
+    if(this.isUser) {
+      console.log('user has logged in.');
+      localStorage.setItem('isUser', 'true');
+    }
+    else {
+      console.log('kitchen has logged in.');
+      localStorage.setItem('isUser', 'false');
+    }
     console.log('Getting local storage')
     console.log(localStorage.getItem('currUserToken'))
     console.log(localStorage.getItem('currUserName'))
     console.log(localStorage.getItem('currUserID'))
-
-    // this.router.navigate(['/users']);
+    
+    this.router.navigate(['/kitchen']);
   }
 }

912d8fc1eeb6d8f878151dc982e793f3c40ea52e
Ricardo Gil <contact@gilricardo.com>
2020-07-23 18:59:15 -0400

    Ricardo Kithen

--- src/app/examples/kitchens/kitchens.component.ts
+++ src/app/examples/kitchens/kitchens.component.ts
@@ -11,9 +11,9 @@ export class KitchensComponent implements OnInit {
 
   private listKitchens: Kitchen[] = [
 
-    { "_id": 0, "KitchenName": "Mcdonalds", "Email": "conctact@mcdonalds.com", "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "./assets/img/faces/mcdonald.jpg", "items": { }},
-    { "_id": 1, "KitchenName": "Wendys", "Email": "conctact@endys.com", "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "./assets/img/faces/wendys.jpg", "items": { } },
-    { "_id": 1, "KitchenName": "Burger King", "Email": "conctact@burgerking.com",  "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "./assets/img/faces/burger.jpg", "items": { } }
+    { "_id": 0, "KitchenName": "Mcdonalds", "Email": "conctact@mcdonalds.com", "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "../../../assets/img/faces/mcdonald.jpg", "items": { }},
+    { "_id": 1, "KitchenName": "Wendys", "Email": "conctact@endys.com", "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "../../../assets/img/faces/wendys.jpg", "items": { } },
+    { "_id": 1, "KitchenName": "Burger King", "Email": "conctact@burgerking.com",  "Password": "123", "KitchenType": 1, "WorkingDays": "All", "OpenTime": "9:00 A.M.", "CloseTime": "10:00 P.M.", "ImagePath": "../../../assets/img/faces/burgerking.jpg", "items": { } }
 
   ];
   private selectedKitchen: Kitchen;
@@ -36,6 +36,18 @@ export class KitchensComponent implements OnInit {
 
     this.selectedKitchen = kitchen;
     console.log(kitchen.KitchenName);
+
+  }
+
+  getSource(kitchen: Kitchen){
+
+    return kitchen.ImagePath;
+
+  }
+
+  kitchenMenu(kitchen: Kitchen){
+
+    console.log("Kitchen Menu");
 
   }
 

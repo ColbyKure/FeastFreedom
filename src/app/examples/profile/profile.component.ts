a6704037f3e390d4168d945c831aaa396cae9e0c
Ricardo Gil <contact@gilricardo.com>
2020-07-24 09:42:44 -0400

    Ricardo Profile Update

--- src/app/examples/profile/profile.component.ts
+++ src/app/examples/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from '../../apiservice.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user'
 
 @Component({
     selector: 'app-profile',
@@ -8,8 +11,55 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProfileComponent implements OnInit {
 
-    constructor() { }
+    private loggedIn : boolean; //
+    private typeUser : boolean; // true = Regular User ; false =  Kitchen
+    private userName : String;
+
+    constructor(private Uservice: ApiserviceService, private router:Router) {
+
+        if(localStorage.getItem('currUserID') != null){
+
+            let promise = this.Uservice.getUser().toPromise();
+            promise.then((data) => {
+
+                for(var i = 0; i < data.length ; i++){
+
+                    if(data[i].UserName == localStorage.getItem('currUserID')){
+
+                        var us: User;
+                        this.userName = us.UserName;
+                        console.log(this.userName);
+
+
+                    }
+
+                }
+                console.log(data);
+            
+
+            }).catch((error) => {
+            
+                console.log("Error getting the user");
+
+            });
+            
+        }
+
+     }
 
     ngOnInit() {}
+
+    isKitchen(){
+
+        if(localStorage.getItem('isKitchen') == "true"){
+
+            return true;
+
+        }else{
+
+            return false;
+        }
+
+    }
 
 }

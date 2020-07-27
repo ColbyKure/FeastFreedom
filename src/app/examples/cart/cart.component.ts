1ab6eda651fb6e3768125c6f15b9463bce0c1535
daiying98 <daiying98@gmail.com>
2020-07-27 11:02:42 -0400

    product

--- src/app/examples/cart/cart.component.ts
+++ src/app/examples/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../models/product';
 import { Item } from '../../models/item';
 import { ProductService } from '../../services/product.service';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-cart',
@@ -22,7 +24,7 @@ export class CartComponent implements OnInit {
 
 	ngOnInit() {
 		this.activatedRoute.params.subscribe(params => {
-			var id = params['id'];
+			var id = params['_id'];
 			if (id) {
 				var item: Item = {
 					product: this.productService.find(id),
@@ -86,5 +88,17 @@ export class CartComponent implements OnInit {
 		localStorage.setItem("cart", JSON.stringify(cart));
 		this.loadCart();
 	}
+
+	exportAsPDF(divId)
+    {
+        let data = document.getElementById('divId');  
+        html2canvas(data).then(canvas => {
+        const contentDataURL = canvas.toDataURL('image/png')  
+        let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
+        // let pdf = new jspdf('p', 'cm', 'a4'); Generates PDF in portrait mode
+        pdf.addImage(contentDataURL, 'PNG', 0, 0, 29.7, 21.0);  
+        pdf.save('Receipt.pdf');
+      }); 
+    }
 }
 
